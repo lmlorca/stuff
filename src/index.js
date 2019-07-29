832462aa54a6cf2509675b46c74fb9d8c328baba
Luis Manuel Lorca <llorca@truenorthcorporation.com>
2019-07-29 15:38:35 -0400

    Added controller

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const express = require('express')
+const app = express()
+
+app.listen(7000, () => console.log('Server running at port 70000'))
---
+++ src/post-controller.js
@@ -0,0 +1,8 @@
+exports.save = async function({ post }) {
+    // Implementation
+    // Write to CSV
+}
+exports.findAll = async function() {
+    // Implementation
+    // Read from CVS
+}
