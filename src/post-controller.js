6d87e0e8fd408955b624161bd6d7f8cf1b3214fd
Luis Manuel Lorca <llorca@truenorthcorporation.com>
2019-07-29 15:46:54 -0400

    Implemented Use Cases in controller.

--- src/post-controller.js
+++ src/post-controller.js
@@ -1,8 +1,11 @@
-exports.save = async function({ post }) {
+const getPosts = require('./get-posts')
+const addPost = require('./add-post')
+
+exports.writePostToCSV = addPost(async function({ post }) {
     // Implementation
     // Write to CSV
-}
-exports.findAll = async function() {
+})
+exports.findAllPostFromCSV = getPosts(async function() {
     // Implementation
     // Read from CVS
-}
+})
